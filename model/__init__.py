e3246adf5c56b91cb0c197ed9f13125b4b64076c
Chi Tinh <tinhnguyen23122004@gmail.com>
2025-07-11 23:23:25 +0700

    Refactor MSCA_Net and RecognitionHead: comment out unused classifiers and outputs to simplify the model structure. Update forward method to focus on fused outputs, enhancing clarity and maintainability. Modify Phoenix test scripts to enable resume functionality for better testing continuity.

--- model/__init__.py
+++ model/__init__.py
@@ -10,41 +10,45 @@ class RecognitionHead(nn.Module):
     def __init__(self, cfg, gloss_tokenizer):
         super().__init__()
 
-        self.left_gloss_classifier = nn.Linear(
+        # self.left_gloss_classifier = nn.Linear(
+        #     cfg["residual_blocks"][-1], len(gloss_tokenizer)
+        # )
+        # self.right_gloss_classifier = nn.Linear(
+        #     cfg["residual_blocks"][-1], len(gloss_tokenizer)
+        # )
+        # self.body_gloss_classifier = nn.Linear(
+        #     cfg["residual_blocks"][-1], len(gloss_tokenizer)
+        # )
+        self.fuse_gloss_classifier = nn.Linear(
             cfg["residual_blocks"][-1], len(gloss_tokenizer)
         )
-        self.right_gloss_classifier = nn.Linear(
-            cfg["residual_blocks"][-1], len(gloss_tokenizer)
-        )
-        self.body_gloss_classifier = nn.Linear(
-            cfg["residual_blocks"][-1], len(gloss_tokenizer)
-        )
-
         self.fuse_alignment_head = FeatureAlignment(
             **cfg["fuse_alignment"], cls_num=len(gloss_tokenizer)
         )
 
     def forward(
         self,
-        left_output,
-        right_output,
+        # left_output,
+        # right_output,
         fuse_output,
-        body_output,
+        # body_output,
     ):
-        left_output = left_output.permute(2, 0, 1)
-        right_output = right_output.permute(2, 0, 1)
-        body_output = body_output.permute(2, 0, 1)
+        # left_output = left_output.permute(2, 0, 1)
+        # right_output = right_output.permute(2, 0, 1)
+        # body_output = body_output.permute(2, 0, 1)
         fuse_output = fuse_output.permute(2, 0, 1)
 
-        left_logits = self.left_gloss_classifier(left_output)
-        right_logits = self.right_gloss_classifier(right_output)
+        # left_logits = self.left_gloss_classifier(left_output)
+        # right_logits = self.right_gloss_classifier(right_output)
+        # body_logits = self.body_gloss_classifier(body_output)
         fuse_logits = self.fuse_alignment_head(fuse_output)
-        body_logits = self.body_gloss_classifier(body_output)
+        fuse_coord_logits = self.fuse_gloss_classifier(fuse_output)
         outputs = {
             "fuse_gloss_logits": fuse_logits,
-            "left_gloss_logits": left_logits,
-            "right_gloss_logits": right_logits,
-            "body_gloss_logits": body_logits,
+            "fuse_coord_logits": fuse_coord_logits,
+            # "left_gloss_logits": left_logits,
+            # "right_gloss_logits": right_logits,
+            # "body_gloss_logits": body_logits,
         }
 
         return outputs
@@ -61,17 +65,20 @@ class MSCA_Net(torch.nn.Module):
 
         self.self_distillation = cfg["self_distillation"]
 
-        self.body_encoder = SeparativeCoordinateAttention(
-            cfg["body_idx"], num_frame=cfg["num_frame"], cfg=cfg
-        )
-        self.left_encoder = SeparativeCoordinateAttention(
-            cfg["left_idx"], num_frame=cfg["num_frame"], cfg=cfg
-        )
-        self.right_encoder = SeparativeCoordinateAttention(
-            cfg["right_idx"], num_frame=cfg["num_frame"], cfg=cfg
-        )
-        self.coordinates_fusion = CoordinatesFusion(
-            cfg["residual_blocks"][-1], cfg["residual_blocks"][-1], 0.2
+        # self.body_encoder = SeparativeCoordinateAttention(
+        #     cfg["body_idx"], num_frame=cfg["num_frame"], cfg=cfg
+        # )
+        # self.left_encoder = SeparativeCoordinateAttention(
+        #     cfg["left_idx"], num_frame=cfg["num_frame"], cfg=cfg
+        # )
+        # self.right_encoder = SeparativeCoordinateAttention(
+        #     cfg["right_idx"], num_frame=cfg["num_frame"], cfg=cfg
+        # )
+        # self.coordinates_fusion = CoordinatesFusion(
+        #     cfg["residual_blocks"][-1], cfg["residual_blocks"][-1], 0.2
+        # )
+        self.keypoint_encoder = SeparativeCoordinateAttention(
+            cfg["fuse_idx"], num_frame=cfg["num_frame"], cfg=cfg
         )
         self.recognition_head = RecognitionHead(cfg, gloss_tokenizer)
 
@@ -85,36 +92,37 @@ class MSCA_Net(torch.nn.Module):
                 for k, v in src_input.items()
             }
 
-        keypoints = src_input["keypoints"]
-        mask = src_input["mask"]
+        # keypoints = src_input["keypoints"]
+        # mask = src_input["mask"]
 
-        body_embed = self.body_encoder(
-            keypoints[:, :, self.cfg["body_idx"], :], mask
-        )  # (B, D, T/4)
-        left_embed = self.left_encoder(
-            keypoints[:, :, self.cfg["left_idx"], :], mask
-        )  # (B, D, T/4)
-        right_embed = self.right_encoder(
-            keypoints[:, :, self.cfg["right_idx"], :],
-            mask,  # (B, D, T/4)
-        )
-        fuse_embed = self.coordinates_fusion(
-            left_embed, right_embed, body_embed
-        )  # (B, 3D, T/4)
-        head_outputs = self.recognition_head(
-            left_embed, right_embed, fuse_embed, body_embed
-        )
+        # body_embed = self.body_encoder(
+        #     keypoints[:, :, self.cfg["body_idx"], :], mask
+        # )  # (B, D, T/4)
+        # left_embed = self.left_encoder(
+        #     keypoints[:, :, self.cfg["left_idx"], :], mask
+        # )  # (B, D, T/4)
+        # right_embed = self.right_encoder(
+        #     keypoints[:, :, self.cfg["right_idx"], :],
+        #     mask,  # (B, D, T/4)
+        # )
+        # fuse_embed = self.coordinates_fusion(
+        #     left_embed, right_embed, body_embed
+        # )  # (B, 3D, T/4)
+        keypoints = src_input["keypoints"][:, :, self.cfg["fuse_idx"], :]
+        mask = src_input["mask"]
+        fuse_embed = self.keypoint_encoder(keypoints, mask)
+        head_outputs = self.recognition_head(fuse_embed)
 
         outputs = {**head_outputs, "input_lengths": src_input["valid_len_in"]}
         outputs["total_loss"] = 0
 
-        for k in ["left", "right", "body", "fuse"]:
+        for k in ["fuse"]:
             if torch.isnan(outputs[f"{k}_gloss_logits"]).any():
                 raise ValueError(f"NaN in {k}_gloss_logits")
             if torch.isinf(outputs[f"{k}_gloss_logits"]).any():
                 raise ValueError(f"inf in {k}_gloss_logits")
 
-        for k in ["left", "right", "body", "fuse"]:
+        for k in ["fuse"]:
             l = self.compute_loss(
                 labels=src_input["gloss_labels"],
                 lengths=src_input["gloss_lengths"],
@@ -132,27 +140,37 @@ class MSCA_Net(torch.nn.Module):
                 outputs[f"{k}_loss"] = l
                 outputs["total_loss"] += outputs[f"{k}_loss"]
 
+        outputs["fuse_coord_loss"] = self.compute_loss(
+            labels=src_input["gloss_labels"],
+            lengths=src_input["gloss_lengths"],
+            logits=outputs["fuse_coord_logits"],
+            input_lengths=torch.full(
+                (outputs["fuse_coord_logits"].shape[1],),
+                outputs["fuse_coord_logits"].shape[0],
+            ),
+        )
+        outputs["total_loss"] += outputs["fuse_coord_loss"]
+
         if self.self_distillation:
-            for student, weight in self.cfg["distillation_weight"].items():
-                teacher_logits = outputs["fuse_gloss_logits"]
-                teacher_logits = teacher_logits.detach()
-                student_logits = outputs[f"{student}_gloss_logits"]
+            teacher_logits = outputs["fuse_gloss_logits"]
+            teacher_logits = teacher_logits.detach()
+            student_logits = outputs["fuse_coord_logits"]
 
-                if torch.isnan(student_logits).any():
-                    raise ValueError("NaN in student_logits")
+            if torch.isnan(student_logits).any():
+                raise ValueError("NaN in student_logits")
 
-                if torch.isnan(teacher_logits).any():
-                    raise ValueError("NaN in teacher_logits")
+            if torch.isnan(teacher_logits).any():
+                raise ValueError("NaN in teacher_logits")
 
-                outputs[f"{student}_distill_loss"] = weight * self.distillation_loss(
-                    student_logits, teacher_logits, use_blank=False
-                )
-                if torch.isnan(outputs[f"{student}_distill_loss"]) or torch.isinf(
-                    outputs[f"{student}_distill_loss"]
-                ):
-                    raise ValueError(f"NaN or inf in {student}_distill_loss")
+            outputs["fuse_distill_loss"] = self.distillation_loss(
+                student_logits, teacher_logits, use_blank=False
+            )
+            if torch.isnan(outputs["fuse_distill_loss"]) or torch.isinf(
+                outputs["fuse_distill_loss"]
+            ):
+                raise ValueError("NaN or inf in fuse_distill_loss")
 
-                outputs["total_loss"] += outputs[f"{student}_distill_loss"]
+            outputs["total_loss"] += outputs["fuse_distill_loss"]
 
         return outputs
 
