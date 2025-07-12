dc2267d0ab5b9aadb66eec23c06ad2232652eea5
Chi Tinh <tinhnguyen23122004@gmail.com>
2025-07-12 22:22:11 +0700

    Update NaN loss handling in opt.py: replace exception raising with a print statement and assign a default value to loss, improving training stability and debugging during loss computation.

--- opt.py
+++ opt.py
@@ -31,7 +31,8 @@ def train_one_epoch(
         loss_value = loss.item()
         with torch.autograd.set_detect_anomaly(True):
             if torch.isnan(loss) or torch.isinf(loss):
-                raise ValueError("NaN loss")
+                print("NaN loss")
+                loss = torch.tensor(100)
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
